b0327b0dc4f0871db87ae7fe345844af6b81dc92
Michael  Wheeler <mwheeler2244@yahoo.com>
2024-06-26 20:14:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//   $("#menu").click(function () {
+//     $(this).toggleclass("fas fa-times");
+//     $(".navbar").toggleclass("nav-toggle");
+//   });
+// });
